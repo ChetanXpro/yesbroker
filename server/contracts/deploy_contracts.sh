980d016a3b0b39577dcde37f0db17428376df73b
Mehul Chauhan <imehul24@gmail.com>
2025-09-28 01:37:08 +0530

    added alloy calls

--- server/contracts/deploy_contracts.sh
+++ server/contracts/deploy_contracts.sh
@@ -9,3 +9,9 @@ forge create --rpc-url http://localhost:8545 \
   --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 \
   src/PropertyVerifier.sol:CertificateVerifier --broadcast \
   --constructor-args 0xEE469e23285a6447851eE1a66e400199aC40f779 0x00a19121185617661899f275d6e1de8c40382a2e2023f5f362712d6dccc16775
+
+# verifier then programvkey
+forge create --rpc-url https://ethereum-sepolia-rpc.publicnode.com \
+  --private-key 0x0354321fcc872c69dd207ec478fd6781c9c2e52d086ca3d0646901b0e177cdd3 \
+  src/PropertyVerifier.sol:CertificateVerifier --broadcast \
+  --constructor-args 0x397A5f7f3dBd538f23DE225B51f532c34448dA9B 0x00a19121185617661899f275d6e1de8c40382a2e2023f5f362712d6dccc16775
